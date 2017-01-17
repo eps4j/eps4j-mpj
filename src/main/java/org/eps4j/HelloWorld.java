362d8c67a77312745f9805ebf954f4c63b51b1c5
Arnaud Malapert <arnaud.malapert@unice.fr>
2017-01-17 23:51:01 +0100

    create project structure

---
+++ src/main/java/org/eps4j/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * This file is part of eps4j-core, http://github.com/eps4j/eps4j-core
+ *
+ * Copyright (c) 2017, Arnaud Malapert, Université Nice Sophia Antipolis. All rights reserved.
+ *
+ * Licensed under the BSD 3-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
+package org.eps4j;
+
+public class HelloWorld {
+
+    public HelloWorld() {}
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/org/eps4j/HelloTest.java
@@ -0,0 +1,30 @@
+/**
+ * This file is part of eps4j-core, http://github.com/eps4j/eps4j-core
+ *
+ * Copyright (c) 2017, Arnaud Malapert, Université Nice Sophia Antipolis. All rights reserved.
+ *
+ * Licensed under the BSD 3-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
+package org.eps4j;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class HelloTest{
+
+    public HelloTest() {}
+
+    @Test(groups = "1s", timeOut = 60000)
+    public void test1() {
+        assertEquals(2, 2);
+    }
+    
+    @Test(groups = "10s", timeOut = 60000)
+    public void test10() {
+        assertEquals(10, 10);
+    }
+
+    
+}
